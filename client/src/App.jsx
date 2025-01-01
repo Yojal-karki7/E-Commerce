a39fa40fd3256793abdf080e07e2898a8f803256
Yojal-karki7 <yojalkarki@gmail.com>
2025-01-01 18:46:41 +0545

    complete routes and Navbar component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,10 +9,11 @@ import Product from './pages/Product'
 import Login from './pages/Login'
 import PlaceOrder from './pages/PlaceOrder'
 import Orders from './pages/Orders'
-
+import Navbar from './components/NAvbar'
 const App = () => {
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <Navbar/>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/about' element={<About/>}/>
